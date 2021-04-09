32d2e467f8b7cade61f267ac0707421e4f823430
kydyrbekova <a.kydyrbekova@gmail.com>
2021-04-09 14:07:31 +0600

    add task: 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,22 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const apperance = [];
+  const result = [];
+  for (let i = 0; i < str.length; i++) {
+    apperance.push(str.split('').filter((el) => el === str[i]).length);
+  }
+  let i = 0;
+  while (i < str.length) {
+    if (apperance[i] > 1) {
+      result.push(`${apperance[i]}${str[i]}`);
+    } else {
+      result.push(str[i]);
+    }
+    i += apperance[i];
+  }
+  return result.join('');
 }
 
 module.exports = encodeLine;
